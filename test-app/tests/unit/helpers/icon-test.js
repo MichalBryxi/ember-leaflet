7c006d99af8742c51ab6730ae3c9c4f4ace4e78c
Michal Bryxí <michal.bryxi@gmail.com>
2025-04-13 18:06:40 +0200

    feat: npx ember-codemod-v1-to-v2

---
+++ test-app/tests/unit/helpers/icon-test.js
@@ -0,0 +1,15 @@
+import { icon } from 'test-app/helpers/icon';
+import { module, test } from 'qunit';
+/* global L */
+
+module('Unit | Helper | icon', function () {
+  test('it works', function (assert) {
+    let result = icon([], {
+      iconUrl: 'custom-url.png'
+    });
+
+    assert.ok(result);
+    assert.ok(result instanceof L.Icon);
+    assert.strictEqual(result.options.iconUrl, 'custom-url.png');
+  });
+});
