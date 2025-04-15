c91263c98019456ade3b4d79f836e2e7f175252f
Michal Bryxí <michal.bryxi@gmail.com>
2025-04-15 10:57:51 +0200

    fix: Some tiny things, not important

--- test-app/app/app.js
+++ test-app/app/app.js
@@ -1,9 +1,12 @@
 import Application from '@ember/application';
+import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
 import config from 'test-app/config/environment';
 
-console.log('AAA', config);
+if (macroCondition(isDevelopingApp())) {
+  importSync('./deprecation-workflow');
+}
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
