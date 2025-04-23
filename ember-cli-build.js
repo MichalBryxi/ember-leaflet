aea328749b29c807dcfcfd700c3a23a305ca1f01
Michal Bryxí <michal.bryxi@gmail.com>
2025-04-23 10:44:33 +0200

    fix: Asset fingerprinting for tests

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
     fingerprint: {
-      exclude: ['assets/images']
+      exclude: ['assets/images', 'test-support.js', 'tests.js']
     },
     'ember-cli-favicon': {
       faviconsConfig: {
